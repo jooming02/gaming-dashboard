1b8e0c959bc2e5b4dcb5d88e617f3cf280a2925d
jooming02 <laujm02@hotmail.com>
2025-06-18 15:58:45 +0800

    housekeeping

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,9 @@
+// clsx - Conditionally constructs className strings
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+// twMerge - Resolves conflicting Tailwind CSS classes:
+// twMerge('px-2 py-1 px-4') 
+// Returns: "py-1 px-4" (removes conflicting px-2)
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
